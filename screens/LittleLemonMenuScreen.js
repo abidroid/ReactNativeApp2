d263d6072401234747b20f14f679f6a46636d848
Muhammad Abid <abidroid1@gmail.com>
2025-04-19 12:27:27 +0500

    fetch request

--- screens/LittleLemonMenuScreen.js
+++ screens/LittleLemonMenuScreen.js
@@ -1,9 +1,39 @@
-import {View, Text} from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { useState, useEffect } from 'react';
+import React from 'react';
+
 
 
 const LittleLemonMenuScreen = () => {
+    const [isLoading, setLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
 
-    return <View><Text>Little Menu</Text></View>
+
+    const getMenu = async () => {
+
+        const url = 'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json';
+
+        try {
+            const response = await fetch(url);
+
+            const json = await response.json();
+
+            setData(json.menu);
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=> getMenu());
+
+    return <View>
+
+        {isLoading ? <ActivityIndicator /> : <Text>{JSON.stringify(data)}</Text>}
+
+    </View>
 }
 
 export default LittleLemonMenuScreen;
