84867b44ebc25ccacd513d2745e01a3db67944c6
Muhammad Abid <abidroid1@gmail.com>
2025-04-19 12:54:49 +0500

    fixess

--- screens/LittleLemonMenuScreen.js
+++ screens/LittleLemonMenuScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
 import React from 'react';
 
@@ -11,6 +11,7 @@ const LittleLemonMenuScreen = () => {
 
     const getMenu = async () => {
 
+        console.log('executed');
         const url = 'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json';
 
         try {
@@ -29,11 +30,45 @@ const LittleLemonMenuScreen = () => {
 
     useEffect(() => { getMenu() });
 
-    return <View>
 
-        {isLoading ? <ActivityIndicator /> : <Text>{JSON.stringify(data)}</Text>}
+    const Item = ({ data }) => {
+        return (
+            <View style={menuStyles.menuRow}>
+                <Text style={menuStyles.menuItemText}>{data.title}</Text>
+                <Text style={menuStyles.menuItemText}>${data.price}</Text>
+            </View>
+        );
+    };
+
+    const renderItem = ({ item }) => <Item data={item} />
+
+    return <View style={menuStyles.container}>
+
+        {isLoading ? <ActivityIndicator /> :
+
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+            />
+        }
 
     </View>
 }
+
+const menuStyles = StyleSheet.create({
+    container: {flex: 1},
+    menuRow: { 
+        backgroundColor: '#cccccc',
+        margin: 4,
+        padding: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderRadius: 8,
+    },
+    menuItemText: {
+        fontSize: 16,
+    }
+});
+
 
 export default LittleLemonMenuScreen;
