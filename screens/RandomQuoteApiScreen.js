de512fac108052993972e6dc0fd4319d8d8c2e27
Muhammad Abid <abidroid1@gmail.com>
2025-04-21 09:11:06 +0500

    api request

--- screens/RandomQuoteApiScreen.js
+++ screens/RandomQuoteApiScreen.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 const RandomQuoteApiScreen = () => {
+
+    const [isLoading, setLoading] = React.useState(true);
+    const [quote, setQuote] = React.useState({});
+
+    const getRandomQuote = async () => {
+
+        setLoading(true);
+
+        const url = 'https://dummyjson.com/quotes/random';
+
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            setQuote(json);
+        } catch (error) {
+            console.error(error);
+        }finally {
+            setLoading(false);
+        }
+    };
+
+    
 
 }
 
