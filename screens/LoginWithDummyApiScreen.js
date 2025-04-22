18d439dbf47eb46f613d3a99853f75babd6ed1f7
Muhammad Abid <abidroid1@gmail.com>
2025-04-22 13:05:13 +0500

    image added

--- screens/LoginWithDummyApiScreen.js
+++ screens/LoginWithDummyApiScreen.js
@@ -1,13 +1,37 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, StyleSheet, ScrollView, Image} from 'react-native';
 
 const LoginWithDummyApiScreen = () => {
 
     return (
-        <View>
-            <Text>yes</Text>
+        <View style={styles.container}>
+            <ScrollView>
+                <Image
+                source={require('../img/intellilogics.jpg')}/>
+                <Text>Please login to continue</Text>
+
+            </ScrollView>
         </View>
     );
 
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        alignItems: 'center',
+        
+    },
+
+    input: {
+        padding: 8,
+        margin: 8,
+        borderRadius: 12,
+        borderColor: 'black',
+        borderWidth: 1,
+    }, 
+
+    buttonContainer: {},
+    buttonText: {},
+});
 export default LoginWithDummyApiScreen;
