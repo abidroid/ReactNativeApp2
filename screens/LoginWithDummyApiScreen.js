1e4d479f07f8a4c1e9637293e07d3664afd30173
Muhammad Abid <abidroid1@gmail.com>
2025-04-23 11:30:05 +0500

    fixes

--- screens/LoginWithDummyApiScreen.js
+++ screens/LoginWithDummyApiScreen.js
@@ -1,14 +1,34 @@
-import {View, Text, TextInput, StyleSheet, ScrollView, Image} from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, Image, Pressable } from 'react-native';
 
 const LoginWithDummyApiScreen = () => {
 
+    const [email, onChangeEmail] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
     return (
         <View style={styles.container}>
             <ScrollView>
                 <Image
-                source={require('../img/intellilogics.jpg')}/>
-                <Text>Please login to continue</Text>
+                style={styles.logo}
+                    source={require('../img/intellilogics.jpg')} />
+                <Text style={styles.message}>Please login to continue</Text>
 
+                <TextInput
+                    style={styles.input}
+                    placeholder='Email'
+                    onChangeText={onChangeEmail}
+                    value={email}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Password'
+                    onChangeText={onChangePassword}
+                    value={password}
+                />
+
+                <Pressable style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>LOGIN</Text>
+                </Pressable>
             </ScrollView>
         </View>
     );
@@ -19,19 +39,34 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
-        alignItems: 'center',
-        
+        // alignItems: 'center',
+
     },
 
+    logo: {
+        alignSelf: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        marginVertical: 20,
+    },
     input: {
         padding: 8,
-        margin: 8,
+        marginVertical: 8,
         borderRadius: 12,
         borderColor: 'black',
         borderWidth: 1,
-    }, 
+    },
 
-    buttonContainer: {},
-    buttonText: {},
+    buttonContainer: {
+        backgroundColor: 'green',
+        padding: 12,
+        borderRadius: 12,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+
+    },
 });
 export default LoginWithDummyApiScreen;
